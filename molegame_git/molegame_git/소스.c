2ff1c1c2f43cb5c37a9b88629d7889ce7c0302bc
DESKTOP-AJIFIA8\1565c <1565chang@gmail.com>
2024-12-05 03:20:43 +0900

    Add input handler
    Add input handler to process player input for mole positions

--- molegame_git/molegame_git/소스.c
+++ molegame_git/molegame_git/소스.c
@@ -21,6 +21,25 @@ void generateMole(char grid[ROWS][COLS]) {
     grid[x][y] = 'M';
 }
 
+void handleInput(char grid[ROWS][COLS]) {
+    int x, y;
+    printf("Enter coordinates to whack the mole (row and column): ");
+    scanf("%d %d", &x, &y);
+
+    if (x >= 0 && x < ROWS && y >= 0 && y < COLS) {
+        if (grid[x][y] == 'M') {
+            printf("You hit the mole!\n");
+            grid[x][y] = '-';
+        }
+        else {
+            printf("Miss! No mole here.\n");
+        }
+    }
+    else {
+        printf("Invalid coordinates. Try again.\n");
+    }
+}
+
 int main() {
     char grid[ROWS][COLS];
     for (int i = 0; i < ROWS; i++) {
@@ -36,6 +55,10 @@ int main() {
 
     generateMole(grid);
     printf("Mole appeared!\n");
+    displayGrid(grid);
+
+    handleInput(grid);
+    printf("Updated Grid:\n");
     displayGrid(grid);
 
     return 0;
